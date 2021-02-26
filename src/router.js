5feaba2ef0a915c11663402b5db28d9974a78182
sumitmandal123 <31107039+sumitmandal123@users.noreply.github.com>
2021-02-26 11:31:42 +0530

    Added login

--- src/router.js
+++ src/router.js
@@ -4,35 +4,38 @@ import Inventory from './component/inventory/Inventory';
 import Product from './component/product/Product';
 import ProductAdd from './component//product/ProductAdd';
 import WareHouse from './component/warehouse/WareHouse';
-import AddWarehouse from './component/warehouse/AddWarehouse';
+import Login from './component/Login/Login';
+import Home from './component/Home/Home';
 
 function Router(params) {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/Dashboard">
-                    <Dashboarad />
-                </Route>
-                <Route path="/Inventory">
-                    <Inventory />
-                </Route>
-                <Route path="/Product">
-                    <Product />
-                </Route>
-                <Route path="/Warehouse">
-                    <WareHouse></WareHouse>
-                </Route>
-                <Route path="/Add/Product">
+        <div>
+            <Home>
+            </Home>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/Dashboard">
+                        <Dashboarad />
+                    </Route>
+                    <Route path="/Inventory">
+                        <Inventory />
+                    </Route>
+                    <Route path="/Product">
+                        <Product />
+                    </Route>
+                    <Route path="/Warehouse">
+                        <WareHouse></WareHouse>
+                    </Route>
+                    <Route path="/Add/Product">
+                        <ProductAdd></ProductAdd>
+                    </Route>
+                    <Route path="/Edit/Product/:id">
                     <ProductAdd></ProductAdd>
-                </Route>
-                <Route path="/Edit/Product/:id">
-                    <ProductAdd></ProductAdd>
-                </Route>
-                <Route path="/Add/WareHouse">
-                    <AddWarehouse />
-                </Route>
-            </Switch>
-        </BrowserRouter>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+
     );
 }
 
